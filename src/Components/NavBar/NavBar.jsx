6f60592ee3462311b41022415aae092035af098b
Agustin <agustin@Laptop-de-Agustin.local>
2024-09-03 20:36:39 -0300

    NavBar 0.2

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import styles from "./NavBar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
 
 const NavBar = () => {
   const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
   const [isDirectorsOpen, setIsDirectorsOpen] = useState(false);
+  const [isUserOpen, setIsUserOpen] = useState(false);
 
   return (
     <div>
       <nav className={styles.container_nav}>
         <img src="../../../Images/logo1.png" alt="" />
         <ul>
-          {/* Menú desplegable para Categorías */}
+          {/* Drop-down menu for Categories */}
           <li
             className={`${styles.list_items} ${styles.dropdown}`}
             onClick={() => setIsCategoriesOpen(!isCategoriesOpen)}
@@ -53,7 +58,7 @@ const NavBar = () => {
             )}
           </li>
 
-          {/* Menú desplegable para Directores */}
+          {/* Drop-down menu for Directors */}
           <li
             className={`${styles.list_items} ${styles.dropdown}`}
             onClick={() => setIsDirectorsOpen(!isDirectorsOpen)}
@@ -86,6 +91,44 @@ const NavBar = () => {
             )}
           </li>
         </ul>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          className={styles.navBarInput}
+        />
+        <button type="submit" className={styles.NavButtons}>
+          <FontAwesomeIcon icon={faSearch} className={styles.awesomeIcons} />
+        </button>
+        <div className={styles.navIconsRight}>
+          <button type="submit" className={styles.NavButtons}>
+            <FontAwesomeIcon icon={faHome} className={styles.awesomeIcons} />
+          </button>
+          <li
+            className={`${styles.list_items} ${styles.dropdown}`}
+            onClick={() => setIsUserOpen(!isUserOpen)}
+          >
+            <button type="submit" className={styles.NavButtons}>
+              <FontAwesomeIcon
+                icon={faUserCircle}
+                className={styles.awesomeIcons}
+              />
+            </button>
+            {isUserOpen && (
+              <ul className={`${styles.dropdown_menu} ${styles.dropdownUser}`}>
+                <li className={styles.list_items}>
+                  <a href="#" className={styles.enlaces}>
+                    Iniciar sesion
+                  </a>
+                </li>
+                <li className={styles.list_items}>
+                  <a href="#" className={styles.enlaces}>
+                    Registrarme
+                  </a>
+                </li>
+              </ul>
+            )}
+          </li>
+        </div>
       </nav>
     </div>
   );
